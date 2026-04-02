c1601a72d50684861b902622a18eea723233c98f
sarojkumarvn <sarojojha2457@gmail.com>
2026-04-02 19:20:36 +0530

    Initialized the repo

---
+++ src/test/java/com/example/devforge/DevforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

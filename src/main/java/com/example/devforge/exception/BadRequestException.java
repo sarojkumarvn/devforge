7ebbc71ddad666b4f4779166566643b3557cbc0b
sarojkumarvn <sarojojha2457@gmail.com>
2026-08-25 15:37:14 +0530

    .

--- src/main/java/com/example/devforge/exception/BadRequestException.java
+++ src/main/java/com/example/devforge/exception/BadRequestException.java
@@ -2,6 +2,7 @@ package com.example.devforge.exception;
 
 public class BadRequestException extends DomainException {
     public BadRequestException(String message) {
-        super(message);
+        super(message); // got the access of the parent class constructor 
+        // and add the message to that 
     }
 }
